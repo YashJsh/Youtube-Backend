f00e220d3004ee9bafdf0959d1b88a3e0a30d5da
yash <yaxj28@gmail.com>
2026-01-21 12:35:18 +0530

    get_channel_videos

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -6,74 +6,88 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import type { Request, Response } from "express"
 
 
-const getChannelStats = asyncHandler(async (req : Request, res : Response) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+const getChannelStats = asyncHandler(async (req: Request, res: Response) => {
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
-    const [
-  subsAggregate,
-  videoAggregate,
-  likesAggregate] = await Promise.all([
-    Subscription.aggregate([
-      {
-        $match : {
-          channel : req.user._id
+  const [
+    subsAggregate,
+    videoAggregate,
+    likesAggregate] = await Promise.all([
+      Subscription.aggregate([
+        {
+          $match: {
+            channel: req.user._id
+          },
         },
-      },
-      {
-        $count : "totalSubscribers"
-      }
-    ]),
-    Video.aggregate([
-      {
-        $match : {
-          owner : req.user._id
+        {
+          $count: "totalSubscribers"
         }
-      },
-      {
-        $count : "totalVideos"
-      }
-    ]),
-    Like.aggregate([
-      {
-        $match : {
-          video : {
-            $exists : true
+      ]),
+      Video.aggregate([
+        {
+          $match: {
+            owner: req.user._id
           }
+        },
+        {
+          $count: "totalVideos"
         }
-      },
-      {
-        $lookup : {
-          from : "videos",
-          localField : "video",
-          foreignField : "_id",
-          as : "video"
+      ]),
+      Like.aggregate([
+        {
+          $match: {
+            video: {
+              $exists: true
+            }
+          }
+        },
+        {
+          $lookup: {
+            from: "videos",
+            localField: "video",
+            foreignField: "_id",
+            as: "video"
+          }
+        },
+        {
+          $unwind: "$video"
+        },
+        {
+          $match: {
+            "video.owner": req.user._id,
+          }
+        },
+        {
+          $count: "totalLikes"
         }
-      },
-      {
-        $unwind : "$video"
-      },
-      {
-        $match : {
-          "video.owner" : req.user._id,
-        }
-      },
-      {
-        $count : "totalLikes"
-      }
-    ])
-  ]);
+      ])
+    ]);
 
   const totalSubscribers = subsAggregate[0]?.totalSubscribers || 0;
   const totalVideos = videoAggregate[0]?.totalVideos || 0;
   const totalLikes = likesAggregate[0]?.totalLikes || 0;
 
-  return res.status(200).json(new APIresponse(200, {totalSubscribers, totalLikes, totalVideos}, "Data fetched successfully"))
-
-
+  return res.status(200).json(new APIresponse(200, { totalSubscribers, totalLikes, totalVideos }, "Data fetched successfully"))
 });
 
-const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+const getChannelVideos = asyncHandler(async (req: Request, res: Response) => {
+  const channelId = req.user._id;
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const videos = await Video.find({
+    owner: channelId,
+    isPublished: true,
+  })
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit);
+
+  return res.status(200).json(
+    new APIresponse(200, { videos }, "Videos fetched successfully")
+  );
 });
 
-export {getChannelStats, getChannelVideos}
+export { getChannelStats, getChannelVideos }
